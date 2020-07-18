bfd7b2b6280585f795fb1786c9619fd6e4df2c18
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-18 09:08:11 +0300

    todo service base dirs added & todo entit added

---
+++ todo-service/src/main/java/com/sakinramazan/todoassistant/todoservice/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.sakinramazan.todoassistant.todoservice.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+    @GetMapping("{sample.url}")
+    public String getCenteralizeConfigSreverTest() {
+        return "Hello from Git server";
+    }
+
+    @GetMapping("test")
+    public String getTest() {
+        return "Hello from Test api";
+    }
+}
---
+++ todo-service/src/main/java/com/sakinramazan/todoassistant/todoservice/entity/Todo.java
@@ -0,0 +1,31 @@
+package com.sakinramazan.todoassistant.todoservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "todo")
+public class Todo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //    @NotNull(message = "{validation.messages.airport.name}")
+    private String headline;
+
+    private String details;
+
+    // Transient tells "do not persist this field"
+//    @Transient
+//    private List<Address> addresses;
+}
