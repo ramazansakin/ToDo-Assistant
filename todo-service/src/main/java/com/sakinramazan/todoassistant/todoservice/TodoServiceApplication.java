49100baa8400d27833637c731cf7173850e86131
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-18 08:50:30 +0300

    Sample todo service added

--- todo-service/src/main/java/com/sakinramazan/todoassistant/todoservice/TodoServiceApplication.java
+++ todo-service/src/main/java/com/sakinramazan/todoassistant/todoservice/TodoServiceApplication.java
@@ -2,12 +2,29 @@ package com.sakinramazan.todoassistant.todoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//@EnableEurekaClient
 @SpringBootApplication
+@RestController
+@RefreshScope
 public class TodoServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoServiceApplication.class, args);
+    }
+
+
+    @GetMapping("{sample.url}")
+    public String getCenteralizeConfigSreverTest() {
+        return "Hello from Git server";
+    }
+
+    @GetMapping("test")
+    public String getTest() {
+        return "Hello from Test api";
+    }
 
 }
--- todo-service/src/test/java/com/sakinramazan/todoassistant/todoservice/TodoServiceApplicationTests.java
+++ todo-service/src/test/java/com/sakinramazan/todoassistant/todoservice/TodoServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sakinramazan.todoassistant.todoservice;
+//package com.sakinramazan.todoassistant.todoservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TodoServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TodoServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
