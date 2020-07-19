7f5154a7d346fb8c426dcd3cb115a11fa46f56fb
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-19 15:44:12 +0300

    Gateway enabled via Zuul and Pre & Post req filter added

---
+++ gateway-service/src/main/java/com/sakinramazan/todoassistant/gatewayservice/filter/ZuulPreFilter.java
@@ -0,0 +1,38 @@
+package com.sakinramazan.todoassistant.gatewayservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulPreFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(ZuulPreFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("PreFilter: " + String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+}
