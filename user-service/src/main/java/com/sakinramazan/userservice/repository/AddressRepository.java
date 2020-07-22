ca29629f09e2e3656995f0a9a76469ed2c2c5278
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-22 11:16:27 +0300

    AddressRepository some JPQL additions

--- user-service/src/main/java/com/sakinramazan/userservice/repository/AddressRepository.java
+++ user-service/src/main/java/com/sakinramazan/userservice/repository/AddressRepository.java
@@ -4,6 +4,13 @@ import com.sakinramazan.userservice.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
+    List<Address> getAllByCity(String city);
+
+    List<Address> getAllByBuildingNoAndDoorNo(Integer buildingNo, Integer doorNo);
+
+    Address getFirstByCityContaining(String city_prefix);
 }
