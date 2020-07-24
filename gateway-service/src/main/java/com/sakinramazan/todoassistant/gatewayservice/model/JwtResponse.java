512dbf4d01a8adfe4087423b6d9f1545b44786f7
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-24 13:17:22 +0300

    clean up

--- gateway-service/src/main/java/com/sakinramazan/todoassistant/gatewayservice/model/JwtResponse.java
+++ gateway-service/src/main/java/com/sakinramazan/todoassistant/gatewayservice/model/JwtResponse.java
@@ -1,6 +1,5 @@
 package com.sakinramazan.todoassistant.gatewayservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -8,7 +7,6 @@ import java.io.Serializable;
 
 @Data
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class JwtResponse implements Serializable {
 
     private final String jwttoken;
