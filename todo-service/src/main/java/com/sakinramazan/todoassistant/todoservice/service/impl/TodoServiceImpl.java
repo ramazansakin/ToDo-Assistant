52ea524ad52d7fe7f7c485d89ebe1e9446cabdef
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-24 12:50:48 +0300

    JWT configs and enable security for gateway

--- todo-service/src/main/java/com/sakinramazan/todoassistant/todoservice/service/impl/TodoServiceImpl.java
+++ todo-service/src/main/java/com/sakinramazan/todoassistant/todoservice/service/impl/TodoServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class TodoServiceImpl implements     TodoService {
+public class TodoServiceImpl implements TodoService {
 
     private final TodoRepository todoRepository;
 
