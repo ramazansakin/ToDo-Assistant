b3c7a936b9d366d37d638860bcfaeee71f1613d3
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-24 12:49:35 +0300

    Generic exception handler centralized exceptions

--- gateway-service/src/main/java/com/sakinramazan/todoassistant/gatewayservice/handler/GenericExceptionHandler.java
+++ gateway-service/src/main/java/com/sakinramazan/todoassistant/gatewayservice/handler/GenericExceptionHandler.java
@@ -2,15 +2,40 @@ package com.sakinramazan.todoassistant.gatewayservice.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GenericExceptionHandler {
-    // If not found specific exception, use this
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Object> exception(RuntimeException exception) {
-        return new ResponseEntity<>("Generic Exception details : " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private Map<String, String> responseDetails = new HashMap<>();
+
+    @ExceptionHandler({
+            UsernameNotFoundException.class,
+            InvocationTargetException.class,
+            BadCredentialsException.class
+    })
+    public ResponseEntity<Object> exception(Exception exception) {
+        buildResponse(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(responseDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    // If not found specific exception, use this as Internal server error
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> default_exception(Exception exception) {
+        buildResponse(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(responseDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private void buildResponse(String cause, HttpStatus status) {
+        responseDetails.put("Cause", cause);
+        responseDetails.put("Status", status.toString());
     }
 
 }
