9f2bb1dd6f484ac9347bbdb2fea67691fbb1cb6f
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-26 19:36:47 +0300

    Todo consumer service init

---
+++ todo-consumer-service/src/main/java/com/sakinramamzan/todoconsumerservice/TodoConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sakinramamzan.todoconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoConsumerServiceApplication.class, args);
+	}
+
+}
---
+++ todo-consumer-service/src/test/java/com/sakinramamzan/todoconsumerservice/TodoConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakinramamzan.todoconsumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
