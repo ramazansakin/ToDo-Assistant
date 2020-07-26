500c38e48bddaff2408be803ac3eafb4fb6b47f2
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-26 19:35:48 +0300

    Todo producer service init

---
+++ todo-producer-service/src/main/java/com/sakinramamzan/todoproducerservice/TodoProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sakinramamzan.todoproducerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoProducerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoProducerServiceApplication.class, args);
+	}
+
+}
