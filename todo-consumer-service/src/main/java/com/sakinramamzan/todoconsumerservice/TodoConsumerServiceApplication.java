b5103d5da36808c345c731a12ff94d8a7da79fd6
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-27 13:17:55 +0300

    enabled eureka client for consumer service

--- todo-consumer-service/src/main/java/com/sakinramamzan/todoconsumerservice/TodoConsumerServiceApplication.java
+++ todo-consumer-service/src/main/java/com/sakinramamzan/todoconsumerservice/TodoConsumerServiceApplication.java
@@ -2,12 +2,14 @@ package com.sakinramamzan.todoconsumerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TodoConsumerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoConsumerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoConsumerServiceApplication.class, args);
+    }
 
 }
