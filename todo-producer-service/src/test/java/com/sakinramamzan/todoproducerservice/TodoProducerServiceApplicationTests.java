d7cfd0a26ed51aca073d4fd27ff4f480a5ad38e7
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-27 13:16:35 +0300

    commented out unnecessary test file for now

--- todo-producer-service/src/test/java/com/sakinramamzan/todoproducerservice/TodoProducerServiceApplicationTests.java
+++ todo-producer-service/src/test/java/com/sakinramamzan/todoproducerservice/TodoProducerServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sakinramamzan.todoproducerservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TodoProducerServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.sakinramamzan.todoproducerservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TodoProducerServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
