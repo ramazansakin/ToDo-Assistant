0a034e40b573af022e4dc47ab499e4286e783104
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-07 13:47:26 +0300

    clean up

--- todo-assistant-app-config-server/src/main/java/com/sakinramazan/todoassistant/todoassistantappconfigserver/TodoAssistantAppConfigServerApplication.java
+++ todo-assistant-app-config-server/src/main/java/com/sakinramazan/todoassistant/todoassistantappconfigserver/TodoAssistantAppConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.sakinramazan.todoassistant.todoassistantappconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class TodoAssistantAppConfigServerApplication {
 
     public static void main(String[] args) {
